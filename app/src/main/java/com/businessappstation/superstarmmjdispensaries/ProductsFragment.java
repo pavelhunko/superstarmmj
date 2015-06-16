1e2467a86ad5287e3d4f2d38b625a194b6b9e60f
Pavel Hunko <pavelhunko@gmail.com>
2015-06-16 16:00:32 +0300

    products fragment cleanup and refactor

--- app/src/main/java/com/businessappstation/superstarmmjdispensaries/ProductsFragment.java
+++ app/src/main/java/com/businessappstation/superstarmmjdispensaries/ProductsFragment.java
@@ -5,19 +5,19 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 
-public class ProductsFragment extends Fragment {
+public class ProductsFragment extends Fragment implements View.OnClickListener {
     View CBDLiqRow, PremLiqRow, VaporPenRow, VaporPenAccsRow, ClothRow;
-    //ListView lView; create listView instead
-    Uri webpage;
-    Intent webIntent;
+
+    private static String TAG = "products-fragment";
 
     public ProductsFragment() {
-        // Required empty public constructor
+
     }
 
 
@@ -26,14 +26,17 @@ public class ProductsFragment extends Fragment {
                              Bundle savedInstanceState) {
 
         View rootView = inflater.inflate(R.layout.fragment_products_list, container, false);
-        //lView = rootView.findViewById(R.id.)
-        viewsInstantiate(rootView);
-        viewsClickListeners();
-
         return rootView;
     }
 
-    private void viewsInstantiate(View v){
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        viewsInstantiate(view);
+        viewsClickListeners();
+    }
+
+    private void viewsInstantiate(View v) {
         CBDLiqRow = v.findViewById(R.id.cbd_eliquid_row);
         PremLiqRow = v.findViewById(R.id.premium_liquid_row);
         VaporPenRow = v.findViewById(R.id.vapor_pen_row);
@@ -41,56 +44,49 @@ public class ProductsFragment extends Fragment {
         ClothRow = v.findViewById(R.id.cloth_apprl_row);
     }
 
-    private void viewsClickListeners(){
-
-        CBDLiqRow.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                webpage = Uri.parse(getResources().getString(R.string.web_link_cbd));
-                webIntent = new Intent(Intent.ACTION_VIEW, webpage);
-                startActivity(webIntent);
-            }
-        });
-        PremLiqRow.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                webpage = Uri.parse(getResources().getString(R.string.web_link_eliq));
-                webIntent = new Intent(Intent.ACTION_VIEW, webpage);
-                startActivity(webIntent);
-            }
-        });
-        VaporPenRow.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                webpage = Uri.parse(getResources().getString(R.string.web_link_vppen));
-                webIntent = new Intent(Intent.ACTION_VIEW, webpage);
-                startActivity(webIntent);
-            }
-        });
-        VaporPenAccsRow.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                webpage = Uri.parse(getResources().getString(R.string.web_link_vppen_accs));
-                webIntent = new Intent(Intent.ACTION_VIEW, webpage);
-                startActivity(webIntent);
-            }
-        });
-        ClothRow.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                webpage = Uri.parse(getResources().getString(R.string.web_link_clothing));
-                webIntent = new Intent(Intent.ACTION_VIEW, webpage);
-                startActivity(webIntent);
-            }
-        });
+    private void viewsClickListeners() {
+        CBDLiqRow.setOnClickListener(this);
+        PremLiqRow.setOnClickListener(this);
+        VaporPenRow.setOnClickListener(this);
+        VaporPenAccsRow.setOnClickListener(this);
+        ClothRow.setOnClickListener(this);
     }
 
-    @Override
+   /* @Override
     public void onDestroyView() {
         super.onDestroyView();
         Fragment f = getFragmentManager().findFragmentById(R.id.products_list_layout);
-        if (f != null){
+        if (f != null) {
             getFragmentManager().beginTransaction().remove(f).commit();
+        }
+    }*/
+
+    @Override
+    public void onClick(View v) {
+        Log.i(TAG, "onClick entered");
+
+        Intent webIntent = new Intent();
+        Uri webpage = null;
+
+        webIntent.setAction(Intent.ACTION_VIEW);
+
+        switch (v.getId()){
+            case R.id.cbd_eliquid_row:
+                webpage = Uri.parse(getResources().getString(R.string.web_link_vppen_accs));
+            case R.id.premium_liquid_row:
+                webpage = Uri.parse(getResources().getString(R.string.web_link_eliq));
+            case R.id.vapor_pen_row:
+                webpage = Uri.parse(getResources().getString(R.string.web_link_vppen));
+            case R.id.vapor_pen_accs_row:
+                webpage = Uri.parse(getResources().getString(R.string.web_link_vppen_accs));
+            case R.id.cloth_apprl_row:
+                webpage = Uri.parse(getResources().getString(R.string.web_link_clothing));
+
+        }
+        //????
+        if (!webpage.equals(null)) {
+            webIntent.setData(webpage);
+            startActivity(webIntent);
         }
     }
 }
